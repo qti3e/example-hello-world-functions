9c3f437e27442d6e2e0c1b5f730ef159e599acc1
Parsa <i@parsa.ooo>
2025-02-12 10:08:42 -0500

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,6 +1,6 @@
 export default () => {
     const envs = JSON.stringify(Deno.env.toObject(), null, 2);
-    new Response("env: " + envs);
+    return new Response("env: " + envs);
 }
 
 export const config = { path: "/test" };
